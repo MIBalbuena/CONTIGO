0b5ee74aa20c44957b87f851c0b1cbb11d455962
DanitzaKewley <88954155+DanitzaKewley@users.noreply.github.com>
2022-04-19 17:46:43 -0500

    Add files via upload

---
+++ ProyectoInt/js/iniciarSesion.js
@@ -0,0 +1,18 @@
+console.log ("hola");
+const togglePassword = document.querySelector("#togglePassword");
+        const password = document.querySelector("#password");
+
+        togglePassword.addEventListener("click", function (e) {
+            // toggle the type attribute
+            const type = password.getAttribute("type") === "password" ? "text" : "password";
+            password.setAttribute("type", type);
+            
+            // toggle the icon
+            this.classList.toggle("bi-eye");
+        });
+
+        // prevent form submit
+        const form = document.querySelector("form");
+        form.addEventListener('submit', function (e) {
+            e.preventDefault();
+        });
